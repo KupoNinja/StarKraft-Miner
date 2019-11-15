0a0499f481761b6075649b6287ebceb7ff976ade
JJ Garlitos <jj.garlitos@outlook.com>
2019-11-14 19:05:53 -0700

    Added to upgrade objects.

--- app/main.js
+++ app/main.js
@@ -4,23 +4,37 @@ let mineralCounter = document.getElementById("mineral-counter");
 let upgrades = {
   SCV: {
     name: "SCV",
-    count: 0
+    price: 50,
+    quantity: 0,
+    multiplier: 1
   },
   Probe: {
     name: "Probe",
-    count: 0
+    price: 120,
+    quantity: 0,
+    multiplier: 20
   },
   Reaver: {
     name: "Reaver",
-    count: 0
+    price: 450,
+    quantity: 0,
+    multiplier: 50
   },
   Ultralisk: {
     name: "Ultralisk",
-    count: 0
+    price: 1020,
+    quantity: 0,
+    multiplier: 100
   }
 };
 
 function mine() {
   minerals++;
+  updateMineralCounter();
+}
+
+function updateMineralCounter() {
   mineralCounter.innerText = minerals.toString();
 }
+
+function addUpgrade() {}
